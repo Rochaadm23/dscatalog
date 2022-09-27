dd7cc6e09f713e41c6b7e330361b091b755b152b
Fernando Rocha <ribeiro.ribeiro821@gmail.com>
2022-09-27 00:10:05 -0300

    Transação e sessão JPA

--- backend/src/main/java/com/dscatalog/services/CatergoryService.java
+++ backend/src/main/java/com/dscatalog/services/CatergoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dscatalog.entities.Category;
 import com.dscatalog.repositories.CategoryRepository;
@@ -13,7 +14,8 @@ public class CatergoryService {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
-
+	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 
 		return categoryRepository.findAll();
